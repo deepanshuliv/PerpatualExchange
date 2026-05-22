48c1add7daf75ce5139e940dbe577e898851a4aa
sainideep1234 <yashmalkhan545@gmail.com>
2026-05-22 19:18:16 +0530

    added limit and market ordder

--- apps/engine/class/orderBook.ts
+++ apps/engine/class/orderBook.ts
@@ -1,17 +1,18 @@
-import { markdown } from "bun";
+import { isMainThread, markdown } from "bun";
 import { OrderedMap } from "js-sdsl";
-import { type Fills, type Kind, type MARKET, type Order, type OrderBook, type Status, type Type} from "types"
+import { Base } from "js-sdsl/dist/esm/container/ContainerBase";
+import { type Bids, type FillInfo, type Fills, type Kind, type MARKET, type openOrder, type Order, type OrderBook, type Orderdetails, type Status, type Type} from "types"
 
 
 export default class  OrderBookManager{
     private orderBook : OrderBook;
     private fills : Fills[]
-    private orders : Order[]
+    private orders : Order
 
     constructor(){
         this.orderBook = {};
         this.fills = [];
-        this.orders = []
+        this.orders = new Map();
     }
     
 /*
@@ -68,28 +69,322 @@ export type Fills = {
  }
 
 */
-    createUserOrder(userId:string , kind :Kind , type :Type , qty:number , price :number , margin :number ){
-        const OrderToPush :Order = {
-            userId,
-            type,
-            qty,
-            price,  
-            ordeId:crypto.randomUUID(),
-            status:"OPEN",
-            margin,
-            kind, 
-            createdAt:new Date()
+    
+
+
+    createLimitLongOrder(userId:string , kind :Kind , type :Type , qty:number , price :number , margin :number , market :MARKET){
+        const currentOrder = this.createUserOrder(userId , kind , type , qty , margin , price );
+       
+        let fillInfo : FillInfo[] = []
+        
+        const oppSide = this.getOppositeSide(market , kind);
+        let remianingQty = qty;
+        
+        while(remianingQty > 0 && oppSide?.front()){
+            const [bestPrice , PriceLevel] =   oppSide?.front()!;
+            if(bestPrice <= price){
+                if(PriceLevel.totalqty >= qty){
+                    // eatas much as you can 
+                    while(PriceLevel.openOrder[0] && remianingQty > 0 ){
+                        let topOrder = PriceLevel.openOrder[0]!;
+                        const priceLevelRemianingQty = topOrder.totalQty - topOrder.filledQty;
+                        const priceLevelMaxFill = Math.min(priceLevelRemianingQty , remianingQty);
+                        remianingQty-=priceLevelMaxFill;
+                        topOrder.filledQty +=priceLevelMaxFill;
+                        fillInfo.push({price:bestPrice , qty : priceLevelMaxFill});
+                        if(remianingQty === 0 ){
+                            this.addToFills(userId , topOrder.userId , priceLevelMaxFill , bestPrice , currentOrder.orderId , currentOrder.data.type, currentOrder.data.kind , "FILLED" )
+                            this.changeOrderStatus(userId , currentOrder.orderId , "FILLED")
+                        }else{
+                            this.addToFills(userId , topOrder.userId , priceLevelMaxFill , bestPrice , currentOrder.orderId , currentOrder.data.type, currentOrder.data.kind , "PARTIALLY_FILLED" )
+                            this.changeOrderStatus(userId , currentOrder.orderId , "PARTIALLY_FILLED")
+                        }
+                        if(topOrder.filledQty === topOrder.totalQty){
+                            this.addToFills(userId , topOrder.userId , priceLevelMaxFill , bestPrice , topOrder.orderId , currentOrder.data.type, currentOrder.data.kind , "FILLED" )
+                            this.changeOrderStatus(topOrder.userId , topOrder.orderId , "FILLED")
+                            PriceLevel.openOrder.shift();
+                        }else{
+                            this.addToFills(userId , topOrder.userId , priceLevelMaxFill , bestPrice , topOrder.orderId , currentOrder.data.type, currentOrder.data.kind , "PARTIALLY_FILLED" )
+                            this.changeOrderStatus(userId , topOrder.orderId , "PARTIALLY_FILLED")
+                        }
+
+                    }
+                    const currentOrderFilledQty = currentOrder.data.qty - remianingQty;
+                    PriceLevel.totalqty -= currentOrderFilledQty;
+                }
+            
+            }
+            if(PriceLevel.totalqty===0){
+                oppSide.eraseElementByKey(bestPrice);
+            }
         }
-        this.orders.push(OrderToPush)
-        return OrderToPush
+        if(remianingQty === 0){
+            return {fills :fillInfo , orderId:currentOrder.orderId , filleQty:currentOrder.data.qty }
+        }
+        if( type === "LIMIT" ){
+
+            // sit on same side 
+            const sameSide = this.getSameSide(market , kind);;
+            const sameSideOpenOrderDetail :openOrder= {
+                filledQty:0,
+                totalQty:remianingQty, 
+                orderId:currentOrder.orderId, 
+                userId:currentOrder.data.userId
+            }
+            const alreadyPriceOrder = sameSide?.getElementByKey(currentOrder.data.price);
+            if(alreadyPriceOrder){
+                alreadyPriceOrder.totalqty += remianingQty;
+                alreadyPriceOrder.openOrder.push(sameSideOpenOrderDetail)
+                sameSide?.setElement(currentOrder.data.price , alreadyPriceOrder);
+            }
+            // creaet new entry 
+            const newBid : Bids = { totalqty:0 , openOrder:[sameSideOpenOrderDetail]}
+            sameSide?.setElement(currentOrder.data.price , newBid)
+            // if kind market return with as much filled
+        }
+        return {
+            filledQty : remianingQty, 
+            fills: fillInfo,
+            orderId:currentOrder.orderId
+        }
+    }
+
+    createLimitShortOrder(userId:string , kind :Kind , type :Type , qty:number , price :number , margin :number , market :MARKET){
+        const currrentOrder =  this.createUserOrder(userId , kind , type , qty , margin  ,price  );
+        const fillInfo : FillInfo[] = [];
+
+        const oppSide = this.getOppositeSide(market , kind );
+
+        let remianingQty = qty;
+
+        while(remianingQty > 0 && oppSide?.front()){
+            const [bestPrice , PriceLevel ] = oppSide.front()!;
+            if(bestPrice >= price){
+                if(PriceLevel.totalqty >= qty){
+                    const topOrder = PriceLevel.openOrder[0];
+                    while(topOrder && remianingQty>0){
+                        const remainingPriceLevelQty =topOrder.totalQty - topOrder.filledQty ;
+                        const maxQtyFillPriceLevel = Math.min(remainingPriceLevelQty , remianingQty);
+                        remianingQty-=maxQtyFillPriceLevel;
+                        topOrder.filledQty+=maxQtyFillPriceLevel;
+                        fillInfo.push({
+                            price:bestPrice, 
+                            qty : maxQtyFillPriceLevel
+                        })
+                        if(topOrder.filledQty === topOrder.totalQty){
+                            this.addToFills(topOrder.userId , currrentOrder.data.userId , maxQtyFillPriceLevel , bestPrice ,topOrder.orderId ,  currrentOrder.data.type , "LONG" , "FILLED" );
+                            this.changeOrderStatus(topOrder.userId , topOrder.orderId , "FILLED");
+                            PriceLevel.openOrder.shift()
+                        }else{
+                            this.addToFills(topOrder.userId , currrentOrder.data.userId , maxQtyFillPriceLevel , bestPrice ,topOrder.orderId ,  currrentOrder.data.type ,"LONG", "PARTIALLY_FILLED" );
+                            this.changeOrderStatus(topOrder.userId , topOrder.orderId , "PARTIALLY_FILLED");
+                        }
+                        if(remianingQty === 0 ){
+                            this.addToFills(topOrder.userId , currrentOrder.data.userId , currrentOrder.data.qty , currrentOrder.data.price ,currrentOrder.orderId ,  currrentOrder.data.type , "SHORT", "FILLED" );
+                            this.changeOrderStatus(currrentOrder.data.userId , currrentOrder.orderId , "FILLED");
+                        }else{
+                            this.addToFills(topOrder.userId , currrentOrder.data.userId , currrentOrder.data.qty , currrentOrder.data.price ,currrentOrder.orderId ,  currrentOrder.data.type , "SHORT", "PARTIALLY_FILLED" );
+                            this.changeOrderStatus(currrentOrder.data.userId , currrentOrder.orderId , "PARTIALLY_FILLED");
+                        }
+
+                        PriceLevel.totalqty-=maxQtyFillPriceLevel;
+
+                    }       
+              
+
+                }
+            }
+            if(PriceLevel.totalqty === 0){
+
+                oppSide.eraseElementByKey(bestPrice);
+            }
+
+        }
+        if(remianingQty === 0 ){
+            return {fills : fillInfo , filledQty : qty - remianingQty , orderId : currrentOrder.orderId , totalQty:currrentOrder.data.qty} 
+        }
+        // sit on same side 
+        if(type === "LIMIT"){
+            const sameSide = this.getSameSide(market , kind);
+            
+            const priceOrder = sameSide?.getElementByKey(price);
+            const pushOpenOrderDetails : openOrder= {
+                filledQty:0, 
+                orderId:currrentOrder.orderId, 
+                totalQty:qty - remianingQty ,
+                userId:currrentOrder.data.userId
+            }
+            if(priceOrder){
+                priceOrder.totalqty+= remianingQty;
+                priceOrder.openOrder.push(pushOpenOrderDetails)
+                sameSide?.setElement(currrentOrder.data.qty , priceOrder)
+            }   
+            // creaet a new price Order
+            const newBid:Bids = {
+                openOrder:[pushOpenOrderDetails],
+                totalqty:qty - remianingQty
+            }
+
+            sameSide?.setElement(currrentOrder.data.price , newBid);
+        }
+
+        /// order cacncel
+        return {
+            filledQty: qty - remianingQty , 
+            totalQty : qty, 
+            orderId:currrentOrder.orderId,
+            fills:fillInfo
+
+        }
+    }
+
+    createMarketLongOrder(userId:string , kind :Kind , type :Type , qty:number ,margin :number , market :MARKET){
+         const currrentOrder = this.createUserOrder(userId , kind, type , qty , margin);
+         const fillInfo : FillInfo[]=[];
+         let remianingQty = qty;
+         const oppSide = this.getOppositeSide(market , kind);
+
+         while(oppSide?.front() && remianingQty>0){
+            const [bestPrice , priceLevel] = oppSide.front()!;
+            
+            const topOrder = priceLevel.openOrder[0];
+            while(topOrder && remianingQty>0){
+                const remainingPriceLevelQty =topOrder.totalQty - topOrder.filledQty ;
+                const maxQtyFillPriceLevel = Math.min(remainingPriceLevelQty , remianingQty);
+                remianingQty-=maxQtyFillPriceLevel;
+                topOrder.filledQty+=maxQtyFillPriceLevel; 
+                fillInfo.push({
+                    price:bestPrice, 
+                    qty : maxQtyFillPriceLevel
+                })
+                if(topOrder.filledQty === topOrder.totalQty){
+                    this.addToFills(userId , topOrder.userId , maxQtyFillPriceLevel , bestPrice ,currrentOrder.orderId ,  currrentOrder.data.type , "SHORT" , "FILLED" );
+                    this.changeOrderStatus(topOrder.userId , topOrder.orderId , "FILLED");
+                    priceLevel.openOrder.shift()
+                    
+                }else{
+                    this.addToFills(userId , topOrder.userId , maxQtyFillPriceLevel , bestPrice ,topOrder.orderId ,  currrentOrder.data.type ,"SHORT", "PARTIALLY_FILLED" );
+                    this.changeOrderStatus(topOrder.userId , topOrder.orderId , "PARTIALLY_FILLED");
+                }
+                if(remianingQty === 0 ){
+                    this.addToFills(userId , topOrder.userId ,currrentOrder.data.qty , bestPrice ,currrentOrder.orderId ,  currrentOrder.data.type , "LONG", "FILLED" );
+                    this.changeOrderStatus(currrentOrder.data.userId , currrentOrder.orderId , "FILLED");
+                }else{
+                    this.addToFills(userId , topOrder.userId , currrentOrder.data.qty , bestPrice ,currrentOrder.orderId ,  currrentOrder.data.type , "LONG", "PARTIALLY_FILLED" );
+                    this.changeOrderStatus(currrentOrder.data.userId , currrentOrder.orderId , "PARTIALLY_FILLED");
+                }
+                priceLevel.totalqty -= maxQtyFillPriceLevel;
+            }
+            if(priceLevel.totalqty === 0){
+                oppSide.eraseElementByKey(bestPrice);
+            }
+            
+         }
+
+         // right now return  the order 
+         // but in future , apply funding rate here
+
+         return {
+            filledQty : qty - remianingQty,
+            totalQty : qty,
+            fills:fillInfo,
+            orderId : currrentOrder.orderId
+         }
+    }
+
+
+
+    createMarketShortOrder(userId:string , kind :Kind , type :Type , qty:number ,margin :number , market :MARKET){
+         const currrentOrder = this.createUserOrder(userId , kind, type , qty , margin);
+         const fillInfo : FillInfo[]=[];
+         let remianingQty = qty;
+         const oppSide = this.getOppositeSide(market , kind);
+
+         while(oppSide?.front() && remianingQty>0){
+            const [bestPrice , priceLevel] = oppSide.front()!;
+            
+            const topOrder = priceLevel.openOrder[0];
+            while(topOrder && remianingQty>0){
+                const remainingPriceLevelQty =topOrder.totalQty - topOrder.filledQty ;
+                const maxQtyFillPriceLevel = Math.min(remainingPriceLevelQty , remianingQty);
+                remianingQty-=maxQtyFillPriceLevel;
+                topOrder.filledQty+=maxQtyFillPriceLevel; 
+                fillInfo.push({
+                    price:bestPrice, 
+                    qty : maxQtyFillPriceLevel
+                })
+                if(topOrder.filledQty === topOrder.totalQty){
+                    this.addToFills(userId , topOrder.userId , maxQtyFillPriceLevel , bestPrice ,currrentOrder.orderId ,  currrentOrder.data.type , "SHORT" , "FILLED" );
+                    this.changeOrderStatus(topOrder.userId , topOrder.orderId , "FILLED");
+                    priceLevel.openOrder.shift()
+                    
+                }else{
+                    this.addToFills(userId , topOrder.userId , maxQtyFillPriceLevel , bestPrice ,topOrder.orderId ,  currrentOrder.data.type ,"SHORT", "PARTIALLY_FILLED" );
+                    this.changeOrderStatus(topOrder.userId , topOrder.orderId , "PARTIALLY_FILLED");
+                }
+                if(remianingQty === 0 ){
+                    this.addToFills(userId , topOrder.userId ,currrentOrder.data.qty , bestPrice ,currrentOrder.orderId ,  currrentOrder.data.type , "LONG", "FILLED" );
+                    this.changeOrderStatus(currrentOrder.data.userId , currrentOrder.orderId , "FILLED");
+                }else{
+                    this.addToFills(userId , topOrder.userId , currrentOrder.data.qty , bestPrice ,currrentOrder.orderId ,  currrentOrder.data.type , "LONG", "PARTIALLY_FILLED" );
+                    this.changeOrderStatus(currrentOrder.data.userId , currrentOrder.orderId , "PARTIALLY_FILLED");
+                }
+                priceLevel.totalqty -= maxQtyFillPriceLevel;
+            }
+            if(priceLevel.totalqty === 0){
+                oppSide.eraseElementByKey(bestPrice);
+            }
+            
+         }
+
+         // right now return  the order 
+         // but in future , apply funding rate here
+
+         return {
+            filledQty : qty - remianingQty,
+            totalQty : qty,
+            fills:fillInfo,
+            orderId : currrentOrder.orderId
+         }
+    }
+
+
+
+    createUserOrder(userId:string , kind :Kind , type :Type , qty:number ,  margin :number ,price? :number ,){
+    const orderId = crypto.randomUUID()
+            const OrderToPush :Orderdetails = {
+                userId,
+                type,
+                qty,
+                price : price === undefined ? 0 : price,  
+                status:"OPEN",
+                margin,
+                kind, 
+                createdAt:new Date()
+            }
+            this.orders.set(orderId , OrderToPush);
+
+            return {orderId ,data: OrderToPush}
     }
 
     getOppositeSide( market : MARKET , kind:Kind){
         const marketPresent  = this.orderBook[market];
         if(!marketPresent){
-            return {ok:false , msg:"MARKET_IS_NOT_AVAILABEL"}
+        return null;
         }
-        return marketPresent
+        const oppPos = kind === "LONG" ? "asks" : "bids";
+
+        return marketPresent[oppPos];
+    }
+
+    getSameSide( market : MARKET , kind:Kind){
+        const marketPresent  = this.orderBook[market];
+        if(!marketPresent){
+        return null;
+        }
+        const samePos = kind === "LONG" ? "bids" : "asks";
+
+        return marketPresent[samePos];
     }
 
     intializedMarket(market:MARKET ){
@@ -103,18 +398,16 @@ export type Fills = {
         return marketCreate
     }
 
- 
 
-    createLimitLongOrder(userId:string , kind :Kind , type :Type , qty:number , price :number , margin :number ){
+
+    cancelOrder(userId : string , orderId:string ){
+        // find and delete in open orders that order 
+        // chnage the order status to cancelled in orders array
+
+    this.getOrder(userId , orderId);    
 
     }
 
-    createLimitShortOrder(){}
-
-    createMarketLongOrder(){}
-
-    createMarketOrder(){}
-    
     addToFills(buyerId : string , sellerId : string ,qty:number , price:number , orderId:string , type:Type , kind:Kind  ,status:Status){
         // orderId is for one either seller or buyer 
         // status is either "FILLED" and "PARTIALLY_FLLED"
@@ -130,27 +423,24 @@ export type Fills = {
             createdAt:new Date()
         }
         this.fills.push(fillDetail);
-        return {ok:true , msg:"FILLS_CREATED_SUCCESFULLY"}
     }
 
     getOrder(userId : string , orderId:string){
-        const userOrder = this.orders.find((order)=>{
-            return order.orderId === orderId && userId === order.userId;
-        })
+        const userOrder = this.orders.get(orderId);
         if(!userOrder){
-            return {ok:false , msg:"USER_ORDER_NOT_AVAILABEL"}
+            return null
         }
         return userOrder
     };
 
     changeOrderStatus( userId:string , orderId :string , status : Status){
-        const userOrder =this.orders.find((order)=> (order.orderId === orderId && userId === userId));
-        if(!userOrder){
-            return {ok:false , msg:"USER_ORDER_NOT_AVAILABEL"}
+        const tempOrder=this.orders.get(orderId);
+        if(tempOrder?.userId !== userId ){
+            return null
         }
-
-        userOrder.status = status
-        return {ok:true , msg:"STATUS_CHNAGED_SUCCESSFULLY_OF_ORDER"}
+        tempOrder.status = status
+        this.orders.set(orderId , tempOrder);
+        
     }
 
     pushOrder(userId:string , orderId:string){
